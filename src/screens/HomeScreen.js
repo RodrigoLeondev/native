fe3ee423c51a3cf5ddfce19cfc83f82e05d9c382
Rodrigo <rodrigoleon.dev@gmail.com>
2024-06-20 19:53:14 -0600

    create button and touchable opacity

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,42 @@
-import React from "react";
-import { Text, StyleSheet } from "react-native";
+import React from 'react'
+import { Text, StyleSheet, Button, View, TouchableOpacity } from 'react-native'
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
-};
+  return (
+    <View>
+      <Text style={styles.text}>Home</Text>
+      <Button
+        title="Go to Components"
+        onPress={() => console.log('press corrected')}
+      />
+      <TouchableOpacity
+        onPress={() => console.log('press corrected')}
+        style={styles.buttonStyle}
+      >
+        <Text style={styles.textStyle}>Go to Components</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 30,
+    textAlign: 'center',
+    color: 'blue',
+    marginTop: 10,
+    marginBottom: 10,
   },
-});
+  buttonStyle: {
+    backgroundColor: 'blue',
+    padding: 10,
+    marginTop: 5,
+  },
+  textStyle: {
+    fontSize: 20,
+    textAlign: 'center',
+    color: 'white',
+  },
+})
 
-export default HomeScreen;
+export default HomeScreen
